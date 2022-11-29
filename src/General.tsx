ee8501f155ddd872c663d6adb09612b7f60749f6
voskanvl <voskanvl@gmail.com>
2022-11-29 14:53:25 +0300

    forward & bakward move pages

--- src/General.tsx
+++ src/General.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate, Route, Routes } from "react-router";
 import { useTransition, animated } from "react-spring";
 import Global from "./globalConts";
@@ -7,8 +7,21 @@ type ForwardBackward = "forward" | "backward";
 
 export default function General() {
     const location = useLocation();
+    const { pathname, state } = location;
+
+    console.log("🚀 ~ location", location);
     const navigate = useNavigate();
     const [direction, setDirection] = useState<ForwardBackward>("forward");
+
+    useEffect(() => {
+        const currentIndex = Global.pages.find(e => e.path === pathname)?.id;
+        const previousIndex = Global.pages.find(e => e.path === state.path)?.id;
+
+        if (!currentIndex || !previousIndex) return;
+
+        if (currentIndex < previousIndex) setDirection("backward");
+        else setDirection("forward");
+    }, [location]);
 
     const calcTransitionConfig = () => ({
         from: {
